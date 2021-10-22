0d105c93e46300495d8398b976e7ceca53840c4e
Brandon Lee <sirbrandonn@berkeley.edu>
2021-10-21 21:36:54 -0700

    Created folder of test files and worked on project_Image

--- project_Image.py
+++ project_Image.py
@@ -1,5 +1,42 @@
-from PIL import Image
+from PIL import Image as PImage
+from glob import glob
+from os import listdir
 
-tempImage = Image.open("/Users/brandon/Downloads/test.jpg")
 
-tempImage.show()
+path = "/Users/brandon/Desktop/Holobox/Project_Holo/TestFiles/"
+
+def loadFiles(path):
+    # return array of files
+
+    fileList = listdir(path)
+    loadedFiles = []
+    for file in fileList:
+        currentFile = PImage.open(path + file)
+        loadedFiles.append(currentFile)
+
+    return loadedFiles
+
+fileArray = loadFiles(path)
+
+for currentFile in fileArray:
+    currentFile.show()
+
+
+
+#types = ["*.gif", "*.png", "*.jpg", "*.mp4", "*.mp3", "*.mov"]
+
+# fileTypeArray = []
+
+#for type in types:
+#    thisFileType = glob.glob(type)
+#    fileTypeArray += thisFileType
+# "/Users/brandon/Downloads/test.jpg"
+#    tempImage = Image.open(type)
+#    tempImage.show()
+
+# print(fileTypeArray)
+
+
+
+
+
